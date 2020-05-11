a88095743a57ed63de70749d3d2b10a348094008
KailashDeval <kailash.deval@gmail.com>
2020-05-11 07:44:09 -0500

    Update Task.bat

--- Task.bat
+++ Task.bat
@@ -10,4 +10,4 @@ curl -O https://en.wikipedia.org/wiki/Google_Search#/media/File:Google_Homepage.
 
 echo Task 3
 
-copy File_Google_Homepage.svg https://bp-vsts.visualstudio.com/GRIP-OC/_build/results?buildId=389586&view=JFrog.jfrog-artifactory-vsts-extension.artifactory-build-info-tab 
+Echo " Completed Successfully"
